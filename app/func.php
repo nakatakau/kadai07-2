73a0f1bd9f2c091b1989a7868bf388c097e5b6a9
nakamura takuma <takuma.nakamura1131@gmail.com>
2021-10-25 19:22:23 +0900

    修正

--- app/func.php
+++ app/func.php
@@ -6,10 +6,10 @@
 function pdo()
 {
   try {
-    $db_name = $_SERVER['SERVER_NAME'] == "localhost" ? "ec_test" : "nt-lab11-19_ec_test";   //データベース名
-    $db_id   = $_SERVER['SERVER_NAME'] == "localhost" ? "root" : "nt-lab11-19";              //アカウント名
-    $db_pw   = $_SERVER['SERVER_NAME'] == "localhost" ? "root" : "ec_test1234";              //パスワード：XAMPPはパスワード無しに修正してください。
-    $db_host = $_SERVER['SERVER_NAME'] == "localhost" ? "localhost" : "mysql57.nt-lab11-19.sakura.ne.jp";  //DBホスト
+    $db_name = $_SERVER['SERVER_NAME'] == "localhost" ? "ec_test" : "************************";   //データベース名
+    $db_id   = $_SERVER['SERVER_NAME'] == "localhost" ? "root" : "************************";              //アカウント名
+    $db_pw   = $_SERVER['SERVER_NAME'] == "localhost" ? "root" : "************************";              //パスワード：XAMPPはパスワード無しに修正してください。
+    $db_host = $_SERVER['SERVER_NAME'] == "localhost" ? "localhost" : "************************";  //DBホスト
     return new PDO('mysql:dbname=' . $db_name . ';charset=utf8;host=' . $db_host, $db_id, $db_pw);
   } catch (PDOException $e) {
     exit('DB Connection Error:' . $e->getMessage());
